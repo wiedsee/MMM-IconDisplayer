97b51684a6e74d21b009ab7a15b6b737a0fda7d7
Wietse <110336704+wiedsee@users.noreply.github.com>
2023-01-16 21:17:56 +0100

    Add files via upload

---
+++ MMM-test.js
@@ -0,0 +1,69 @@
+/* Magic Mirror
+ * Module: IconDisplayer
+ * 
+ * By fruestueck based on github.com/MichMich/MagicMirror/blob/master/modules/default/currentweather
+ */
+Module.register("MMM-test", {
+	// Default module config.
+	defaults: {		
+		lang: config.language,
+		preferences: [
+			{
+				topic: "Tank_1", // topic name for the MQTT message
+				icon: "10", // icon name to display
+				max: 10, // maximum value to display icon
+				min: 10, // minimum value to display icon
+			},
+			{
+				topic: "Tank_2", // topic name for the MQTT message
+				icon: "20", // icon name to display
+				max: 10, // maximum value to display icon
+				min: 10, // minimum value to display icon
+			},
+		],
+		
+	},
+	
+	// Define required translations.
+	getTranslations: function() {
+		return false;
+	},
+	
+	// Define start sequence
+	start: function() {
+		Log.info("Starting module: " + this.name); // log the start of the module 
+		this.loaded = false;
+	},
+	// notification received from other module
+	notificationReceived: function(notification, payload, sender) {
+		for (var f in this.config.preferences) {
+			if (notification == this.config.preferences[f].topic) {
+				this.loaded = true;
+				window['topic:'+this.config.preferences[f].topic] = payload;
+				this.updateDom();
+			}
+		}
+	},
+	// create the dom element 	
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		
+		if (!this.loaded) {
+			wrapper.innerHTML = this.translate("LOADING");
+			return wrapper;
+		}
+		
+		for (var x in this.config.preferences) {
+			var icon = this.config.preferences[x];
+			if (icon.min <= window['topic:'+this.config.preferences[x].topic]) {
+				if (icon.max >= window['topic:'+this.config.preferences[x].topic]) {
+					var img = document.createElement("img");
+					img.setAttribute('src',this.data.path + "icons/" + this.config.preferences[x].icon + ".png");
+					img.setAttribute('style', "height: 75px; width: auto; display: inline;");
+					wrapper.appendChild(img);
+				}
+			}
+		} 
+		return wrapper;
+	},
+});
